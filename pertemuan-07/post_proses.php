0324efa581085f7dd7c3a52acec767ba7b25e387
yozalia ilmandra <2511500067@mahasiswa.atmaluhur.ac.id>
2025-11-12 22:25:16 -0800

    mengcopy index.php pertemuan 06 ke pertemuan 08

--- pertemuan-07/post_proses.php
+++ pertemuan-07/post_proses.php
@@ -3,6 +3,6 @@ session_start();
   $_SESSION["nama"] = $_POST["txtNama"];
   $_SESSION["email"] = $_POST["txtEmail"];
   $_SESSION["pesan"] = $_POST["txtPesan"];
-  header(     "Location: post.php");
+ # header(  "Location: post.php");
   echo $_SESSION["nama"] . $_SESSION["email"] . $_SESSION["pesan"];
 ?>
